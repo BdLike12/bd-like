3f1aa51f5cda60981e959012ba4f6acbe8621f4c
greyonion404 <twitterpicker111@gmail.com>
2022-09-30 04:02:42 +0600

    auth0 intialization

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import { useUser } from "@auth0/nextjs-auth0"
+
 export default function Home() {
-  return (
+  const { user, isLoading } = useUser();
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>loading . . .</p>
+      </div>
+    )
+  }
+
+  else if (user) {
+    return (
+      <div>
+        <p>{JSON.stringify(user)}</p>
+        <div>
+          <a href="/api/auth/logout">Logout</a>
+        </div>
+      </div>
+    )
+  }
+
+  else return (
     <div>
-      <p>Hello world</p>
+      <a href="/api/auth/login">Login</a>
     </div>
   )
 }
